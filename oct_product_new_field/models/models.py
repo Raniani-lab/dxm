d60f883c622de675034a17e129deec3ea23eeb00
Pastor Enrique <pastyenriquegmail.com>
2020-08-12 14:36:29 -0400

    uploading module product_new_field

--- oct_product_new_field/models/models.py
+++ oct_product_new_field/models/models.py
@@ -8,3 +8,4 @@ class AddProductField(models.Model):
     _inherit = "product.product"
 
     cantidad_a_mano = fields.Float('Cantidad a Mano', related='qty_available', store=True)
+    cantidad_prevista = fields.Float('Cantidad Prevista', related='virtual_available', store=True)
