bc542f14fcae76e31a50063bff9056766f2d0943
sergio <sergio@webcuba.es>
2020-08-24 22:11:45 +0200

    [fix] stock quant view

--- mobile_device_reception/models/stock_quant.py
+++ mobile_device_reception/models/stock_quant.py
@@ -5,7 +5,7 @@ class StockQuant (models.Model):
     _inherit = 'stock.quant'
 
 
-    revision_grado = fields.Many2one(string="Grado",related="lot_id.x_studio_revision_grade")
+    revision_grado = fields.Many2one(string="Grado",related="lot_id.x_studio_revision_grado")
     
 
 
