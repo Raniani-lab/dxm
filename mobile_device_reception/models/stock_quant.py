90ed15b24992abaa37988111cde7bb80448a6707
sergio <sergio@webcuba.es>
2020-08-24 22:25:15 +0200

    [fix] stock quant view

--- mobile_device_reception/models/stock_quant.py
+++ mobile_device_reception/models/stock_quant.py
@@ -6,6 +6,12 @@ class StockQuant (models.Model):
 
 
     revision_grado = fields.Many2one(string="Grado",related="lot_id.x_studio_revision_grado")
-    
+    color = fields.Many2one(string="Color",related="lot_id.x_studio_color")
+    red = fields.Many2one(string="Red",related="lot_id.x_studio_red")
+    logo = fields.Many2one(string="Logo",related="lot_id.x_studio_logo")
+    idioma = fields.Many2one(string="Idioma",related="lot_id.x_studio_idioma")
+    cargador = fields.Many2one(string="Cargador",related="lot_id.x_studio_cargador")
+    aplicaciones = fields.Many2one(string="Aplicaciones",related="lot_id.x_studio_aplicaciones")
+
 
 
