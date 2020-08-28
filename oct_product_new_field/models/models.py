43f59b2f8bc7999d4cfa66e5eb6c9775f80702cb
Pastor Enrique <pastyenriquegmail.com>
2020-08-28 14:09:03 -0400

    insert new field on product.template view

--- oct_product_new_field/models/models.py
+++ oct_product_new_field/models/models.py
@@ -3,14 +3,20 @@
 
 from odoo import models, fields, api
 
-
-class AddProductField(models.Model):
+"""class AddProductField(models.Model):
     _inherit = "product.product"
 
     cantidad_a_mano = fields.Float('Cantidad a Mano', related='qty_available', store=True)
-    cantidad_prevista = fields.Float('Cantidad Prevista', related='virtual_available', store=True)
+    cantidad_prevista = fields.Float('Cantidad Prevista', related='virtual_available', store=True)"""
+
 
 class AddProductTemplateField(models.Model):
     _inherit = "product.template"
 
     descripcion_website = fields.Text('Descripción para Website', store=True)
+
+#     descripcion_website = fields.Text('Descripción para Website',compute="_des")
+
+#     def _des(self):
+#         for object in self:
+#             object.descripcion_website = object.description_sale
