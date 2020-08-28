394e2a10e40fe806e40d53596426270275af44a1
Pastor Enrique <pastyenriquegmail.com>
2020-08-28 13:34:19 -0400

    insert new field on product.template view

--- oct_product_new_field/models/models.py
+++ oct_product_new_field/models/models.py
@@ -9,3 +9,8 @@ class AddProductField(models.Model):
 
     cantidad_a_mano = fields.Float('Cantidad a Mano', related='qty_available', store=True)
     cantidad_prevista = fields.Float('Cantidad Prevista', related='virtual_available', store=True)
+
+class AddProductTemplateField(models.Model):
+    _inherit = "product.template"
+
+    descripcion_website = fields.Text('Descripción para Website', store=True)
